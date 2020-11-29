75e8deb3cb26816db424ddd4dff1be9b1d11f02a
Segilola Mustapha <>
2020-11-29 14:10:04 +0100

    fixed a bug

--- test.java
+++ test.java
@@ -2,8 +2,9 @@
        
     
   public static void main(String[] args) {
-        for(int i = 0; i< 2; i++){
-            System.out.println("testing "+ i);
+        for(int i = 0; i<= 2; i++){
+            
+            System.out.println(i + 1 + "testing again"+ i );
         }
     }
  }
